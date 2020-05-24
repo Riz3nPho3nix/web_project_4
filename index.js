6d18cb1ca4f5b11a8522976e0229d1cc71c3b518
Brian Wilson <wahooey87@gmail.com>
2020-05-23 20:56:00 -0500

    feat: finish JS, set up BEM and media queries

--- index.js
+++ index.js
@@ -1,45 +1,41 @@
+// Declare variables
 const edit = document.querySelector('.profile__edit');
 const modal = document.querySelector('.modal');
 const modalWindow = document.querySelector('.modal__window');
 const save = document.querySelector('.modal__btn');
-const modalClose = document.querySelector('.close');
-var name = document.querySelector('.profile__name');
-var job = document.querySelector('.profile__job');
-var editName = document.querySelector('.set_name');
-var editJob = document.querySelector('.set_job');
+const modalClose = document.querySelector('.modal__close');
+let name = document.querySelector('.profile__name');
+let job = document.querySelector('.profile__job');
+let editName = document.querySelector('#set_name');
+let editJob = document.querySelector('#set_job');
 
-save.addEventListener('submit',updateProfile);
+// Create Event Listeners
+save.addEventListener('click',updateProfile);
 modalClose.addEventListener('click', closeModal);
 edit.addEventListener('click', openModal);
 
-
+// Create Modal open and close functions
 function closeModal(){
     modal.classList.remove('modal__open');
 }
 
 function openModal(){
     modal.classList.add('modal__open');
-    editName.textContent = name.textContent;
-    editJob.textContent = name.textContent;
+    editName.value = name.textContent;
+    editJob.value = job.textContent;
 }
 
+// Create form completion button
+function updateProfile(e) {
+  // Stop the browser from submitting the form in the default way.
+  e.preventDefault();
 
-function updateProfile(evt) {
-    evt.preventDefault(); // This line stops the browser from submitting the form in the default way.
-    // Having done so, we can define our own way of submitting the form.
-    // We'll explain it in more detail later.
-
-    // Let's find the form fields in the DOM
-
-
-    // Get the values of each field from the corresponding value property
-let newName = editName.textContent;
-let newJob = editJob.textContent;
-    // Select elements where the field values will be entered
-    name.textContent = newName;
-    job.textContent = newJob;
-    // Insert new values using the textContent property of the querySelector() method
-    closeModal();
+  // Get the values of each field from the corresponding value property
+  let newName = editName.value;
+  let newJob = editJob.value;
+  // Insert new values using the textContent property of the querySelector() method
+  name.textContent = newName;
+  job.textContent = newJob;
+  closeModal();
 }
 
-// Connect the handler to the form:
