48e50a7625d6bcc3a25f111979b6c4c4fd03ac6e
Brian Wilson <Brian@iowatech.local>
2020-05-27 17:25:02 -0500

    feat: create new location modal and set up JS

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Declare variables
 const edit = document.querySelector('.profile__edit');
 const modal = document.querySelector('.modal');
-const modalWindow = document.querySelector('.modal__window');
 const save = document.querySelector('.modal__btn');
 const modalClose = document.querySelector('.modal__close');
 const name = document.querySelector('.profile__name');
@@ -9,26 +8,33 @@ const job = document.querySelector('.profile__job');
 const editName = document.querySelector('.set_name');
 const editJob = document.querySelector('.set_job');
 const heart = document.querySelectorAll('.favorite__heart');
-const fave = document.querySelectorAll('.favorite');
+const favorite = document.querySelectorAll('.favorite');
+const favorites = document.querySelector('.favorites');
+const add = document.querySelector('.profile__add-btn');
+const editFave = document.querySelector('.new-favorite');
+const newPic = document.querySelector('.image-path');
+const newName = document.querySelector('.location');
+const newFave = document.querySelector('.new-favorite__btn');
+const closeFave = document.querySelector('.new-favorite__close');
 
 
 // Create Modal open and close functions
 function closeModal(){
-    modal.classList.toggle('overlay__open');
+    modal.classList.toggle('modal__open');
 }
 
 function openModal(){
-    modal.classList.toggle('overlay__open');
+    modal.classList.toggle('modal__open');
     editName.value = name.textContent;
     editJob.value = job.textContent;
 }
 
-function makeFave(heart){
+function toggleHeart(heart){
   heart.classList.toggle('favorite__liked');
 }
 
-function removeFavorite(fave){
-  fave.remove();
+function removeFavorite(favorite){
+  favorite.remove();
 }
 
 // Create form completion button
@@ -42,7 +48,31 @@ function updateProfile(e) {
   closeModal();
 }
 
+function toggleFave(){
+  editFave.classList.toggle('modal__open');
+}
+
+function newFavorite(e){
+  e.preventDefault();
+  favorites.insertAdjacentHTML('beforeend', `
+  <li class="favorite">
+              <div style="background-image: url('${newPic.value}');" class="favorite__image">
+                <button class="favorite__delete-btn" onclick="removeFavorite(this.parentNode.parentNode)"></button>
+              </div>
+                <div class="favorite__label">
+                    <h2 class="favorite__heading">${newName.value}</h2>
+                    <button class="favorite__heart" onclick="toggleHeart(this)"></button>
+                </div>
+            </li>`);
+            newPic.value = '';
+            newName.value = '';
+            toggleFave();
+}
+
 // Create Event Listeners
 save.addEventListener('click',updateProfile);
 modalClose.addEventListener('click', closeModal);
 edit.addEventListener('click', openModal);
+newFave.addEventListener('click', newFavorite);
+add.addEventListener('click', toggleFave);
+closeFave.addEventListener('click', toggleFave);
