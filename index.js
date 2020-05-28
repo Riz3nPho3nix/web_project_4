447621a366ef000e1c07f088ff5b252b6f251465
Brian Wilson <Brian@iowatech.local>
2020-05-28 17:41:35 -0500

    feat: adjust JS for heart and delete buttons

--- index.js
+++ index.js
@@ -16,27 +16,27 @@ const newPic = document.querySelector('.image-path');
 const newName = document.querySelector('.location');
 const newFave = document.querySelector('.new-favorite__btn');
 const closeFave = document.querySelector('.new-favorite__close');
+const deleteFave = document.querySelectorAll('.favorite__delete-btn');
 
 
-// Create Modal open and close functions
+// Close profile modal without saving changes
 function closeModal(){
     modal.classList.toggle('modal__open');
 }
-
+// Open profile modal and set values to form
 function openModal(){
     modal.classList.toggle('modal__open');
     editName.value = name.textContent;
     editJob.value = job.textContent;
 }
-
-function toggleHeart(heart){
-  heart.classList.toggle('favorite__liked');
+// Add and remove full heart on places
+function toggleHeart(){
+  this.classList.toggle('favorite__liked');
 }
-
-function removeFavorite(favorite){
-  favorite.remove();
+// Remove place from list on site
+function removeFavorite(){
+  this.closest('.favorite').remove();
 }
-
 // Create form completion button
 function updateProfile(e) {
   // Stop the browser from submitting the form in the default way.
@@ -47,28 +47,27 @@ function updateProfile(e) {
   job.textContent = editJob.value;
   closeModal();
 }
-
+// Toggle new place modal
 function toggleFave(){
   editFave.classList.toggle('modal__open');
 }
-
+// Add new place to page
 function newFavorite(e){
   e.preventDefault();
   favorites.insertAdjacentHTML('beforeend', `
   <li class="favorite">
               <div style="background-image: url('${newPic.value}');" class="favorite__image">
-                <button class="favorite__delete-btn" onclick="removeFavorite(this.parentNode.parentNode)"></button>
+                <button class="favorite__delete-btn"></button>
               </div>
                 <div class="favorite__label">
                     <h2 class="favorite__heading">${newName.value}</h2>
-                    <button class="favorite__heart" onclick="toggleHeart(this)"></button>
+                    <button class="favorite__heart"></button>
                 </div>
             </li>`);
             newPic.value = '';
             newName.value = '';
             toggleFave();
 }
-
 // Create Event Listeners
 save.addEventListener('click',updateProfile);
 modalClose.addEventListener('click', closeModal);
@@ -76,3 +75,5 @@ edit.addEventListener('click', openModal);
 newFave.addEventListener('click', newFavorite);
 add.addEventListener('click', toggleFave);
 closeFave.addEventListener('click', toggleFave);
+heart.forEach(heart => heart.addEventListener('click', toggleHeart));
+deleteFave.forEach(deleteFave => deleteFave.addEventListener('click', removeFavorite));
